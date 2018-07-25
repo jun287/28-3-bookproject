44819d61c75901e8c9dc776f0b6c53cf1be24113
Administrator <Administrator@192.168.123.18>
2018-07-25 09:10:32 +0900

    [공세준] 1. 매개변수 클래스 명 수정 	( ServiceBook.java )

--- src/service/bookshop/project/ServiceBook.java
+++ src/service/bookshop/project/ServiceBook.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import dao.bookshop.project.BookReviewDao;
-import dto.bookshop.project.Bookreview;
+import dto.bookshop.project.BookReview;
 import util.connetion.db.DBconnection;
 
 public class ServiceBook {
@@ -12,7 +12,7 @@ public class ServiceBook {
 	// 설명 : 책의 리뷰를 데이터베이스에 입력하는 메서드입니다.
 	// 매개변수 : Connection 클래스 타입으로 드라이버로딩, DB연결정보를 담은 connection 객체참조값과 책 리뷰내용이 담긴 Bookreview 클래스객체의 참조값을 받습니다.
 	// 리턴 : void로 없습니다.
-	public void insertBookReview(Bookreview bookReview) {
+	public void insertBookReview(BookReview bookReview) {
 		
 		BookReviewDao bookReviewDao = new BookReviewDao();
 		Connection connection = null;
