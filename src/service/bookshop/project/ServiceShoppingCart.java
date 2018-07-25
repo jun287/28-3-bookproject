55a03a722442070631c7d834c49f748299d00da1
Administrator <Administrator@192.168.123.23>
2018-07-25 10:44:06 +0900

    [이원상] 1. ShoppingCartDao클래스 파일(수정)  1)insertShoppingCart메소드(수정)  	가. 매개변수 추가(Connection connection)  2)deleteShoppingCart메소드(수정)   	가. 매개변수 추가(Connection connection)  3)selectShoppingCartListBypage메소드(수정)   	가. 매개변수 추가(Connection connection)  4)countMemberShoppingCart메소드(수정)   	가. 매개변수 추가(Connection connection)  5)updateShoppingCart메소드(수정)   	가. 매개변수 추가(Connection connection)  6)deleteMemberShoppingCart메소드(생성)  7)인스턴스변수 삭제(Connection connection)  8)생성자내 Connection connection변수 삭제 2. ServiceShoppingCart.java(수정)  1)insertShoppingCart메소드 호출 추가  2)Connection connection인스턴스 변수 추가(DBconnection클래스의 메소드 리턴값)

--- src/service/bookshop/project/ServiceShoppingCart.java
+++ src/service/bookshop/project/ServiceShoppingCart.java
@@ -1,6 +1,38 @@
+// 2018. 7. 25(수) 이원상 ServiceShoppingCart.java
 package service.bookshop.project;
+import dao.bookshop.project.*;
+import dto.bookshop.project.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import dto.bookshop.project.ShoppingCart;
+import util.connetion.db.DBconnection;
 
 public class ServiceShoppingCart {
+	Connection connection=DBconnection.getConnetion();
 	
-		
+	public void insertShoppingCart(ShoppingCart shoppingCart){
+		try {
+			connection.setAutoCommit(false);
+			ShoppingCartDao shoppingCartDao = new ShoppingCartDao();
+			shoppingCartDao.insertShoppingCart(shoppingCart, connection);
+			connection.commit();
+		}catch (SQLException e) {
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}finally {
+			try {
+				connection.setAutoCommit(true);
+				if(connection !=null) {connection.close();}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}	
 }
